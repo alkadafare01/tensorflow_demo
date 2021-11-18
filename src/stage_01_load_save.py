f742a3968227f217a6bd6bb2cbde5663f4bc0658
alkadafare01 <pinki.dafare@gmail.com>
2021-11-18 18:20:54 +0530

    stage 1 added save data

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -18,6 +18,7 @@ logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=lo
 
 def copy_file(source_download_dir, local_data_dir):
     list_of_files = os.listdir(source_download_dir)
+    print(list_of_files)
     N = len(list_of_files)
     for file in tqdm(list_of_files, total=N, desc=f'copying file from {source_download_dir} to {local_data_dir}', colour="green"):
         src = os.path.join(source_download_dir, file)
